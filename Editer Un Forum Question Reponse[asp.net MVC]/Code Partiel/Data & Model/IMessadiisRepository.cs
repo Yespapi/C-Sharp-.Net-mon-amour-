be502f1f2777eff9cfd1ccfa121c0370a236583e
aly ba <alybas2002@yahoo.fr>
2014-04-04 08:17:13 +0200

    mises à jour du  04/04
    forum , asp.net , un peu de link

---
+++ Editer Un Forum Question Reponse[asp.net MVC]/Code Partiel/Data & Model/IMessadiisRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MESSADIIS.Data
+{
+    public interface IMessadiisRepository
+    {
+        IQueryable<Question> GetQuestions();
+        IQueryable<Reponse> GetReponsesByQuestion(int QuestionId);
+    }
+}
