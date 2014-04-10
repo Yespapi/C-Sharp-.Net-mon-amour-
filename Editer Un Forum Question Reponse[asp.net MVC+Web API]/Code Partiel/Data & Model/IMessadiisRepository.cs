0ffcac406c7a900ba3fb9e16d425f40dbcda653c
aly ba <alybas2002@yahoo.fr>
2014-04-10 12:45:08 +0200

    mises a jour reflection

--- Editer Un Forum Question Reponse[asp.net MVC+Web API]/Code Partiel/Data & Model/IMessadiisRepository.cs
+++ Editer Un Forum Question Reponse[asp.net MVC+Web API]/Code Partiel/Data & Model/IMessadiisRepository.cs
@@ -8,6 +8,13 @@ namespace MESSADIIS.Data
     public interface IMessadiisRepository
     {
         IQueryable<Question> GetQuestions();
+        IQueryable<Question> GetQuestionsInclusReponses();
         IQueryable<Reponse> GetReponsesByQuestion(int QuestionId);
+
+        bool Save();
+
+        bool AddQuestion(Question  newQuestion);
+        bool AddReponse(Reponse newReponse);
+
     }
 }
